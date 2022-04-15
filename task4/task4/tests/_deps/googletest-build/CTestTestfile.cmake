80702720a2f80a483da77f923dfe2bd019a57ba2
KseniyaShestakova <shestakova.ko@phystech.edu>
2022-04-15 12:04:19 +0300

    removing some directories

--- task4/task4/tests/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xxeniash/Desktop/TP_Test/TechProgTemplates/task4/tests/_deps/googletest-src
-# Build directory: /home/xxeniash/Desktop/TP_Test/TechProgTemplates/task4/tests/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
